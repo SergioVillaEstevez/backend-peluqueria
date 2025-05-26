8af294bf3df33b1f353552f6e5f226b644a98fed
Sergio Villa Estevez <124885490+SergioVillaEstevez@users.noreply.github.com>
2025-05-26 14:08:10 +0200

    add enlace netlify

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const userRoutes = require('../src/routes/user.routes')
 const PORT =  process.env.PORT ?? 3001;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://barber-villa.netlify.app/',
   credentials: true
 }));
 
